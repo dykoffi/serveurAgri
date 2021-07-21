f1843cfc30b6a610b61cd809e6a51e7b4696ce49
dykoffi <dykoffi2000@gmail.com>
2021-07-21 04:05:22 +0000

    add gitgonre file

--- dockerfile
+++
@@ -1,7 +0,0 @@
-FROM node
-WORKDIR /app
-RUN npm i -g pm2
-COPY package.json /app/
-RUN yarn install
-COPY . .
-CMD ["pm2-runtime", "random.js"]
--- read.js
+++
@@ -1,6 +0,0 @@
-const models = require("./models")
-
-models.light.findAll({ attributes: { exclude: ['id', 'updatedAt'] } })
-    .then(data => {
-        console.log(data.map(d => d.dataValues));
-    })
